73db4827d7ea6fcda5582f5a8bdc7ed68170b0a8
Ubuntu <ubuntu@instance-1.us-west4-b.c.anilvms.internal>
2022-09-20 10:08:50 +0000

    first

---
+++ name2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print("Python scripting")
+import os
+os.system('git init')
+os.system('touch 2.txt')
+os.system('git status')
+os.system('git add .')
+os.system('git status')
+os.system('git commit -m"first"')
+os.system('git status')
+
