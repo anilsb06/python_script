01f0769bf6a02a44944ad8c987e53703dd37d006
Ubuntu <ubuntu@instance-1.us-west4-b.c.anilvms.internal>
2022-09-20 10:21:49 +0000

    second

--- name2.py
+++ name2.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 print("Python scripting")
 import os
-os.system('touch 3.txt')
+os.system('touch 4.txt')
 os.system('git status')
 os.system('git add .')
 os.system('git status')
 os.system('git commit -m"second"')
 os.system('git status')
 os.system('git log')
+os.system('git remote add origin https://github.com/anilsb06/python_script.git')
+os.system('git push origin master')
